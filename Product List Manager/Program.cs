a24a8f537f44634aa65aa3ece18f4031a5ec64b3
Dennis Lindblad <delindblad@gmail.com>
2026-09-03 13:11:57 +0200

    Comitt test.

--- Product List Manager/Program.cs
+++ Product List Manager/Program.cs
@@ -1,18 +1,26 @@
-﻿class ProductListManager
+﻿using System.Text.RegularExpressions;
+class ProductListManager
 {
     static void Main(string[] args)
     {
         Console.WriteLine("-------------------------------------");
-        Console.WriteLine("   PRODUCT LIST MANAGER LEVEL 1");
+        Console.WriteLine("   PRODUCT LIST MANAGER LEVEL 3");
         Console.WriteLine("-------------------------------------");
 
         Console.WriteLine("Enter product names.");
         Console.WriteLine("Type 'exit' to finish.");
+
+        //List of products
         List<string> products = new List<string>();
-        String input;
+        //Input variable
+        string input;
+        //Regex pattern
+        string pattern = @"^([a-zA-Z]+)-([0-9]+)$";
+
+
         Console.Write("Product:");
 
-
+        //Main loop to get user input
         while (true)
         {
             if ((input = Console.ReadLine()) != null)
@@ -23,18 +31,17 @@
             {
                 input = "";
             }
-
-            if (input.IsWhiteSpace())
-            {
-                Console.WriteLine("Whitespace is not allowed.");
-            }
-            else if(input == "exit")
+            if (input == "exit")
             {
                 break;
             }
             else
             {
-                products.Add(input.ToLower().Trim());
+                if (ValidateInput(input, pattern))
+                {
+                    Console.WriteLine("Valid product name.");
+                    products.Add(input);
+                }
             }
             Console.Write("Product:");
 
@@ -47,5 +54,33 @@
             Console.WriteLine("- " + product);
         }
 
+    }
+
+    public static bool ValidateInput(string input, string pattern)
+    {
+        var valid = true;
+
+        //Get match and extract the name and number
+        Match m = Regex.Match(input, pattern);
+        Console.WriteLine("Name:" + m.Groups[1].Value);
+        Console.WriteLine("Number: " +  m.Groups[2].Value);
+
+        if (!m.Success)
+        {
+            valid = false;
+            Console.WriteLine("Invalid product name. Please use the format 'name-number'.");
+        }
+        else
+        {
+
+        }
+
+        if (input.IsWhiteSpace())
+        {
+            valid = false;
+            Console.WriteLine("Input cannot be empty or whitespace.");
+        }
+
+        return valid;
     }
 }
